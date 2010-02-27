a0dbf27f9ac367e067888ea21a9bf64783ad85d8
David O'Hara <david@davidohara.net>
2010-02-27 14:26:48 -0600

    Added a 404 responder Changed up the public folder and added a few more files (img & js)

--- source/Sammy/Program.cs
+++ source/Sammy/Program.cs
@@ -12,8 +12,11 @@ namespace Sammy
 		{
 			KayakServer server = new KayakServer();
 			server.UseFramework();
+
 			server.Start(new System.Net.IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000));
-			server.MapDirectory("/public", Path.Combine(Environment.CurrentDirectory, "../public"));
+			server.MapDirectory("/", Path.Combine(Environment.CurrentDirectory, "../public"));
+			server.AddResponder(new FileNotFoundResponder());
+	
 			Console.WriteLine("Sammy has begun on port 3000");
 			Console.WriteLine("Press Control-Z to exit");
 			ConsoleKeyInfo key = Console.ReadKey();
